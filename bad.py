3be2b98c15a30b6df3ed341af266995738fd4e41
Christopher Ospina <ChrisOspina99@gmail.com>
2024-06-11 09:58:49 -0400

    pre-commit fixed files

--- bad.py
+++ bad.py
@@ -2,11 +2,15 @@ import os
 import re
 import sys
 
-print sys.path
+print(sys.path)
 
-print {'some_key':'lol'}
+print({"some_key": "lol"})
 
-print "a really really super duper long crazy line that goes beyond what is recommended at 80 characters and all of that jazz yay yay yay"
+print(
+    "a really really super duper long crazy line",
+    " that goes beyond what is recommended at 80 characters",
+    " and all of that jazz yay yay yay",
+)
 
 
 def some_func():
